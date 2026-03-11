6d16ccaa6d551c6ca6eeb5efded7e54806a39012
kimkwatson <kimwatson731@gmail.com>
2026-03-11 12:36:10 -0700

    edited swagger.json

---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'WardSignup API',
+        description: 'API for ward signup sheets'
+    },
+    host: 'localhost:3000',
+    schemes: ['http']
+};
+
+const outputFile = './swagger.json';
+const endpointsFile = ['./server.js'];
+
+swaggerAutogen(outputFile, endpointsFile, doc);
