ccd05d0e73b87a772b73f5e8d2bc88eaa747ead4
kimkwatson <kimwatson731@gmail.com>
2026-03-11 12:47:30 -0700

    edited server.js

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 // routes
-router.get("/sheets", async (req, res) => {
+app.get("/sheets", async (req, res) => {
     res.send("All sheets");
 });
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 connectToDb(process.env.MONGODB_URI, process.env.DB_NAME).then(() => {
   app.listen(PORT, () => {
