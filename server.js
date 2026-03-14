c74277a0c7b55cb23d558e9fa6a446af06c8279c
kimkwatson <kimwatson731@gmail.com>
2026-03-14 14:07:07 -0700

    added users tests to .rest

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/sheets", sheets);
 app.use("/users", users);
+app.use("/sheets", sheets);
 app.use("/slots", slots);
 app.use("/claims", claims);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
